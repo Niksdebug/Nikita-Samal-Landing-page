33e87af7bdc49c08eeba66ac89b54a0f3feead82
Nikisdebug <nikitasamal62@gmail.com>
2023-09-05 23:21:58 +0530

    fisst

---
+++ script.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+    
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 40){
+            $('#topbutton').fadeIn();
+        }else{
+            $('#topbutton').fadeOut();
+        } 
+    });
+
+    $("#topbutton").click(function(){
+        $('html ,body').animate({scrollTop : 0},800);
+    });
+});
+$('.carousel').slick({
+    infinite: true, // Enable looping
+    // Other configuration options...
+});
+
+
+
+
+
+var button = document.getElementById('rightside');
+
+    function Togglebar(){
+        button.style.top ="80px";
+    } 
+    function Hidemenu(){
+        button.style.top = "-500px";
+        
+    }
